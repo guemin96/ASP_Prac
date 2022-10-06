9e460cb297c304c19810c51c13b5294b110b6918
PGM <guemin12@naver.com>
2022-10-06 22:46:43 +0900

    20221006 ASP.net 저녁 공부

---
+++ Web_Practice/Web_Practice_2/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Practice_2.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Default");
+        }
+        public IActionResult ContentResultDemo()
+        {
+            return Content("ContentResult 반환값");
+        }
+    }
+}
---
+++ Web_Practice/Web_Practice_2/Controllers/RazorDemoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Practice_2.Controllers
+{
+    public class RazorDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Demo1()
+        {
+            return View();
+        }
+        public IActionResult Demo2()
+        {
+            return View();
+        }
+    }
+}
