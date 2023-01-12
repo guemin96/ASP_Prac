9714770d33b0690afd49567cccc9d7d2ad31b210
guemin <guemin96@naver.com>
2023-01-12 13:50:08 +0900

    commit

---
+++ JSPractice/json.js
@@ -0,0 +1,14 @@
+// Json
+// JavaScript Object Notation
+
+// 1. Object to JSON
+// stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple','banana']);
+console.log(json);
+
+
+// 2. JSON to Object
+// parse(json)
