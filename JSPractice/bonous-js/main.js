c91a05a44c5ae59552e2554fc9a62d106c92778d
guemin <guemin96@naver.com>
2023-01-13 17:53:58 +0900

    commit

---
+++ JSPractice/bonous-js/main.js
@@ -0,0 +1,37 @@
+
+// number, string, boolean, null, undefined, symbol
+let number = 2;
+let number2 = number;
+
+let num = '2';
+
+console.log(number);
+console.log(number2);
+
+number2 =3;
+
+console.log(number);
+console.log(number2);
+
+
+//object
+let obj = {
+  name: 'ellie',
+  age: 5,
+};
+
+let obj2 = obj;
+
+console.log(obj);
+console.log(obj2);
+
+obj2.name= 'gyuemin';
+
+console.log(obj);
+console.log(obj2);
+obj2.test = 'test';
+
+console.log(obj);
+console.log(obj2);
+
+
---
+++ JSPractice/bonous-js/main2.js
@@ -0,0 +1,13 @@
+const num = 1;
+const num2 = 2;
+const result = num + num2;
+console.log(result);
+
+const num3 = 1;
+const num4 = 2;
+const result2 = num3+num4;
+
+function add(num1,num2)
+{
+  return num1+num2;
+}
